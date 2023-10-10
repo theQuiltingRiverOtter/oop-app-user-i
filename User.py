e5c5aa25bce97c0bffb692be2f7105c30b5f2c5f
Megan Buck <thequiltingriverotter@gmail.com>
2023-10-10 12:08:02 -0400

    Added basic class attributes, getters and setters for User class and tests to test basic functions

--- User.py
+++ User.py
@@ -1,1 +1,77 @@
 # your User class goes here
+class User:
+    def __init__(
+        self, name: str, age: int, email: str = None, phone_number: str = None
+    ):
+        self.__name = name
+        self.__age = age
+        self.__email = email
+        self.__phone_number = phone_number
+
+    def __str__(self):
+        if self.__email and self.__phone_number:
+            return f"{self.__name} -  Age: {self.__age}, Email: {self.__email}, Phone Number: {self.__phone_number}"
+        else:
+            return f"{self.__name} - Age: {self.__age}"
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def age(self):
+        return self.__age
+
+    @property
+    def email(self):
+        return self.__email
+
+    @property
+    def phone_number(self):
+        return self.__phone_number
+
+    @name.setter
+    def name(self, name: str):
+        if len(name) < 2:
+            raise ValueError("Name must be at least two characters long")
+        if type(name) != str:
+            raise TypeError("Name must be of type str")
+        else:
+            self.__name = name
+
+    @age.setter
+    def age(self, age: int):
+        if type(age) != int:
+            raise TypeError("Age must be of type int")
+        if age < 0 or age > 120:
+            raise ValueError("Age must be between 0 and 120")
+        else:
+            self.__age = age
+
+    @email.setter
+    def email(self, email: str):
+        if "@" not in email:
+            raise ValueError("email must include an @ symbole")
+        else:
+            self.__email = email
+
+    @phone_number.setter
+    def phone_number(self, phone_number: str):
+        numbers = "0123456789"
+        phone_nums = ""
+        for num in phone_number:
+            if num in numbers:
+                phone_nums += num
+        if len(phone_nums) != 10:
+            raise ValueError("Phone number should be 10 digits long")
+        else:
+            self.__phone_number = phone_number
+
+
+megan = User("Megan", 38, "thequiltingriverotter@gmail.com", "123-456-7890")
+john = User("John", 25, "fakeemail@gmail.com", "978-456-1231")
+bob = User("Bob", 50)
+
+print(megan)
+print(john)
+print(bob)
---
+++ test_user.py
@@ -0,0 +1,45 @@
+from User import User
+import pytest
+
+
+@pytest.fixture
+def user():
+    """Returns a basic user with all values"""
+    return User("Megan", 38, "thequiltingriverotter@gmail.com", "123-456-7890")
+
+
+@pytest.fixture
+def user_without():
+    """Returns a user without email or phone number"""
+    return User("Bob", 50)
+
+
+def test_user_values(user):
+    assert user.name == "Megan"
+    assert user.age == 38
+    assert user.email == "thequiltingriverotter@gmail.com"
+    assert user.phone_number == "123-456-7890"
+
+
+def test_user_without_values(user_without):
+    assert user_without.name == "Bob"
+    assert user_without.age == 50
+    assert user_without.email is None
+    assert user_without.phone_number is None
+
+
+def test_reset_values(user):
+    user.age = 39
+    user.email = "fakeemail@gmail.com"
+    assert user.age == 39
+    assert user.email == "fakeemail@gmail.com"
+
+
+def test_reset_values_raise_type_error(user):
+    with pytest.raises(TypeError):
+        user.age = "39"
+
+
+def test_reset_values_raise_value_error(user):
+    with pytest.raises(ValueError):
+        user.email = "fakeemail"
